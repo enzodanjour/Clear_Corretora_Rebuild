70622d27168a74070e4eb8ff5ddf60a36557c632
Enzo Danjour <enzo.lops999@gmail.com>
2020-05-17 15:55:48 -0300

    Resolve bug in small screens

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -16,91 +16,94 @@ final _registerFormKey = GlobalKey<FormState>();
         centerTitle: true,
         title: Text('Cadastro'),
       ),
-      body: Form(
-        key: _registerFormKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-             TextFormField(
-               decoration: const InputDecoration(
-                 icon: Icon(Icons.person),
-                 hintText: 'Nome completo',
-                 labelText: 'Nome'
+      body: 
+        SingleChildScrollView(
+        child: Form(
+          key: _registerFormKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+               TextFormField(
+                 decoration: const InputDecoration(
+                   icon: Icon(Icons.person),
+                   hintText: 'Nome completo',
+                   labelText: 'Nome'
+                 ),
+                 validator: (value){
+                   if(value.isEmpty){
+                     return 'Preencha o nome';
+                   }
+                   return null;
+                 },
                ),
-               validator: (value){
-                 if(value.isEmpty){
-                   return 'Preencha o nome';
+               TextFormField(
+                 decoration: const InputDecoration(
+                   icon: Icon(Icons.email),
+                   hintText: 'Seu email',
+                   labelText: 'Email'
+                 ),
+                 keyboardType: TextInputType.emailAddress,
+                 validator: (value){
+                   if(value.isEmpty){
+                     return 'Preencha o email';
+                   }
+                   return null;
                  }
-                 return null;
-               },
-             ),
-             TextFormField(
-               decoration: const InputDecoration(
-                 icon: Icon(Icons.email),
-                 hintText: 'Seu email',
-                 labelText: 'Email'
                ),
-               keyboardType: TextInputType.emailAddress,
-               validator: (value){
-                 if(value.isEmpty){
-                   return 'Preencha o email';
-                 }
-                 return null;
-               }
-             ),
-             TextFormField(
-               decoration: const InputDecoration(
-                 icon: Icon(Icons.lock),
-                 hintText: 'Digite uma senha',
-                 labelText: 'Senha'
+               TextFormField(
+                 decoration: const InputDecoration(
+                   icon: Icon(Icons.lock),
+                   hintText: 'Digite uma senha',
+                   labelText: 'Senha'
+                 ),
+                 obscureText: true,
+                 validator: (value){
+                   if(value.isEmpty){
+                     return 'Preencha a senha';
+                   }
+                   return null;
+                 },
                ),
-               obscureText: true,
-               validator: (value){
-                 if(value.isEmpty){
-                   return 'Preencha a senha';
-                 }
-                 return null;
-               },
-             ),
-             TextFormField(
-               decoration: const InputDecoration(
-                 icon: Icon(Icons.lock_outline),
-                 hintText: 'Repita a senha',
-                 labelText: 'Repita a senha'
+               TextFormField(
+                 decoration: const InputDecoration(
+                   icon: Icon(Icons.lock_outline),
+                   hintText: 'Repita a senha',
+                   labelText: 'Repita a senha'
+                 ),
+                 obscureText: true,
+                 validator: (value){
+                   //TODO: corrigir em breve, validação falsa.
+                   if(value.isEmpty){
+                     return'Preencha uma senha';
+                   }
+                   return null;
+                 },
                ),
-               obscureText: true,
-               validator: (value){
-                 //TODO: corrigir em breve, validação falsa.
-                 if(value.isEmpty){
-                   return'Preencha uma senha';
-                 }
-                 return null;
-               },
-             ),
-              Container(
-                margin: const EdgeInsets.only(top:29.0,bottom: 20.0),
-                child: RaisedButton(
-                  child: Text('Enviar'),
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.black,
-                  onPressed: (){
-                    if(_registerFormKey.currentState.validate()){
-                      print('Deu bom');
+                Container(
+                  margin: const EdgeInsets.only(top:29.0,bottom: 20.0),
+                  child: RaisedButton(
+                    child: Text('Enviar'),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.black,
+                    onPressed: (){
+                      if(_registerFormKey.currentState.validate()){
+                        print('Deu bom');
+                      }
                     }
-                  }
+                  ),
                 ),
-              ),
-              Text('Já possui uma conta?'),
-              FlatButton(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/entrar');
-               }, 
-                child: Text('Faça login aqui'))
-            ],
+                Text('Já possui uma conta?'),
+                FlatButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/entrar');
+                 }, 
+                  child: Text('Faça login aqui'))
+              ],
+            ),
+            )
           ),
-          )
-        )
+      )
     );
   }
 }
